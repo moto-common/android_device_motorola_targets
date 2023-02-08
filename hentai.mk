89504257a8c082e3fc1d3a89ffc355c8bf47d2af
electimon <electimon@gmail.com>
2023-02-08 16:29:02 +0800

    targets: Add HentaiOS support
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ hentai.mk
@@ -0,0 +1,37 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# All components inherited here go to system image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
+
+# All components inherited here go to system_ext image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+# All components inherited here go to product image
+$(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
+
+# All components inherited here go to vendor image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+# Device
+DEVICE := $(PRODUCT_DEVICE)
+
+# Kernel
+$(call inherit-product, device/motorola/targets/includes/kernel/prebuilt.mk)
+
+# Hentai Required Packages
+PRODUCT_PACKAGES += libtensorflowlite_jni
