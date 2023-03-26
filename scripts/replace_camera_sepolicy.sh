513b6da0993d28ce424c6516d90b98ac78e35a06
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-03-26 03:18:57 -0500

    fixup
    make it more visually appealing, added comment with the command that's needed to be run

--- scripts/replace_camera_sepolicy.sh
+++ scripts/replace_camera_sepolicy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Replace vendor.camera.aux.packagelist and vendor_persist_camera_prop
+#  ./device/motorola/targets/scripts/replace_camera_sepolicy.sh
 shopt -s extglob
 read -p "Please run this script at the root of your synced source!, Ctrl-C to exit at this point, Enter to continue."
+echo ""
 find device/!(*qcom)/sepolicy -type f -exec sed -i "/vendor.camera.aux.packagelist/s/.*//" {} \;
 if [ $? -eq 0 ]; then
 	echo "Patch 1, Done!"
@@ -14,3 +16,7 @@ if [ $? -eq 0 ]; then
 else
 	echo "Patch 2, Failed..."
 fi
+
+echo ""
+echo "Finished, PRESS ENTER TO EXIT"
+read
