2d01226a70feb695c55c632d8793b36ee8eca903
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-03-26 03:01:47 -0500

    introduce EvolutionX rom

---
+++ rom/evolution.mk
@@ -0,0 +1,35 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Common
+include device/motorola/targets/include/common.mk
+
+# Inherit some common Evolution X stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+TARGET_IS_PIXEL := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# EvolutionX Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS=" "
+
+# Kernel
+$(call inherit-product, device/motorola/targets/include/kernel/source.mk)
+
+# VARIABLE - SePolicy Camera Script
+#$(warning This ROM requires replace_camera_sepolicy.sh to be ran...)
