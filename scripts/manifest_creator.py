5c24b3b0d09f452079c45c92bb309f938e088eea
electimon <electimon@gmail.com>
2023-04-30 13:16:08 +0000

    targets: scripts/manifest_creator: Add more repos to removal keywords
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Ia5eddf0982569fac933985f1a52678a4eeb793ba

--- scripts/manifest_creator.py
+++ scripts/manifest_creator.py
@@ -58,7 +58,13 @@ replacements_keywords = {'qcom-caf/common': {
     "name": "platform_hardware_qcom-caf_common",
     "revision": "master"
 }}
-removals_keywords = ['st-hal', 'data-ipa-cfg-mgr'] + list(replacements_keywords.keys())
+removals_keywords = [
+    'st-hal',
+    'data-ipa-cfg-mgr',
+    'qcom-caf/thermal',
+    'opensource/thermal',
+    'opensource/audio-hal'
+] + list(replacements_keywords.keys())
 
 # Create the new_root, manifest
 new_root = ET.Element('manifest')
