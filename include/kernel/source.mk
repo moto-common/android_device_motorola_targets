667721694d9c9720d7fd6cb1e42fb55b0c7b301d
electimon <electimon@gmail.com>
2023-07-08 02:27:17 +0000

    targets: kernel/source: Build with clang-r450784d and binutils for =>5.4
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Ida7c10fb71930ede1da39854f9bf1efb734d20e1

--- include/kernel/source.mk
+++ include/kernel/source.mk
@@ -12,7 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_KERNEL_CLANG_VERSION := r383902
+ifeq ($(call is-kernel-greater-than-or-equal-to, 5.4),true)
+    TARGET_KERNEL_CLANG_VERSION := r450784d
+    TARGET_KERNEL_LLVM_BINUTILS := true
+else
+    TARGET_KERNEL_CLANG_VERSION := r383902
+    TARGET_KERNEL_LLVM_BINUTILS := false
+endif
 TARGET_KERNEL_CONFIG := vendor/$(DEVICE)_defconfig
-TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_SOURCE := kernel/motorola/msm-$(TARGET_KERNEL_VERSION)
