ff1d64cd182162abe1a65ac80bde3b4772a60200
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-07-08 05:16:39 -0500

    add update rom symlinks script

---
+++ Update_Rom_Symlinks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Updating Rom Symlinks..."
+echo ""
+
+ROM_DIR="rom"
+DEVICE_DIR="devices"
+
+if [ ! -d $ROM_DIR ] || [ ! -d $DEVICE_DIR ]; then
+  echo "Please run this script in device/motorola/targets..."
+  exit 1
+fi
+
+for device in $DEVICE_DIR/*; do
+  for file in $ROM_DIR/*; do
+    ln -sf "../../$file" "$device/$(basename ${file%.*})_$(basename $device).mk"
+  done
+done
+
+echo "Rom Symlinks Updated & Rom Added  =)"
+echo ""
+echo ""
+echo "PRESS ENTER TO EXIT"
+read
