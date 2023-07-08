d32fa44c359fbdd61c8789c4dcfbaca3c5ccc3dd
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-07-08 05:22:45 -0500

    add moto-common repo sync creator

---
+++ scripts/Repo_Sync_Creator/Repo_Sync_Creator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2019-Present A-Team Digital Solutions
+# Copyright (C) 2023 @electimon
+#
+# Moto-Common Repo Sync Creator
+
+python3 A-Team/RSC.py A-Team/moto-common.xml
+
+echo ""
+echo ""
+echo "Repo Sync Command Should Be Made"
+echo ""
+echo ""
+echo "PRESS ENTER TO EXIT"
+read
